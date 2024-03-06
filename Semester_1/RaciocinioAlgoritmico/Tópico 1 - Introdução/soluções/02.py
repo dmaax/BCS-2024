6051757d09a46efee51a64e856fc8735ccb6f1ab
dmaax <dudzcool@gmail.com>
2024-03-06 16:18:46 -0300

    feat: Add second solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções/02.py
@@ -0,0 +1,4 @@
+# Calcule o antecessor e o sucessor de um número
+numero = int(input("Digite um número para imprimir seu antecessor e seu sucessor: "))
+
+print(f"Seu antecessor é {numero - 1} e seu sucessor é {numero + 1}")
