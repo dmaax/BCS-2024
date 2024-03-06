ac99edfaf82fc5b2c7b070a94c216dc80c8f8853
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-06 23:51:26 +0000

    Add tenth solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções/10.py
@@ -0,0 +1,9 @@
+# Calcule a metragem quadrada de uma casa com N pavimentos
+
+largura = 25.3
+comprimento = 50.2
+pavimentos = int(input("Digite o número de pavimentos: "))
+
+area = largura * comprimento * pavimentos
+
+print("A área em metros quadrados é:", area)
