b6cb27e49bae4eb0484221eff72e947c4e7492f5
dmaax <dudzcool@gmail.com>
2024-03-06 16:31:31 -0300

    feat: Add fifth solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções/05.py
@@ -0,0 +1,8 @@
+# Calcule a metragem quadrada de uma área qualquer (casa, terreno, quarto, sala, etc)
+
+largura = 25.3
+comprimento = 50.2
+
+area = largura * comprimento
+
+print("A área em metros quadrados é:", area)
