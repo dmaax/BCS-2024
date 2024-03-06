9c025757cedc017f8800cbaafb53f6c3ec759a0b
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-06 23:48:05 +0000

    feat: Add ninth solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções/09.py
@@ -0,0 +1,8 @@
+# Calcular a gorjta de N%
+
+total_conta = 232.50
+
+gorjeta_porcentagem = int(input("Digite a gorjeta em porcentagem: "))
+gorjeta_valor = total_conta * gorjeta_porcentagem / 100
+
+print("O valor da gorjeta é", gorjeta_valor)
