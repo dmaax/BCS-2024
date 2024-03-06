2912fa493b50f4812cf60221d7def7d5ca0b9ac1
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-06 23:30:43 +0000

    feat: Add sixth solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções/06.py
@@ -0,0 +1,9 @@
+# Calcule a metragem quadrada de uma casa com 3 pavimentos
+
+largura = 25.3
+comprimento = 50.2
+pavimentos = 3
+
+area = largura * comprimento * pavimentos
+
+print("A área em metros quadrados é:", area)
