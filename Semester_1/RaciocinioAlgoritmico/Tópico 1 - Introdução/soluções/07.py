360442cbb05822ba1b41f6504f8f844b58e9304e
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-06 23:35:49 +0000

    feat: Add seventh solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções/07.py
@@ -0,0 +1,9 @@
+# Calcule a média de idade de 5 pessoas
+
+idade1 = 17
+idade2 = 18
+idade3 = 20
+idade4 = 23
+idade5 = 24
+
+print(f"A média das idades é: {(idade1 + idade2 + idade3 + idade4 + idade5)/5}")
