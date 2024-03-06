ad2e716c92b1b7be238a85343ef25e431835f075
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-06 23:40:10 +0000

    feat: Add eighth solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções/08.py
@@ -0,0 +1,10 @@
+# Calcule a idade a partir do ano de nascimento (em anos, meses e dias)
+
+ano_nascimento = 2000
+ano_atual = 2024
+
+idade_anos = ano_atual - ano_nascimento
+idade_meses = idade_anos * 12
+idade_dias = idade_anos * 365
+
+print(f"A idade em anos é: {idade_anos}, em meses: {idade_meses}, em dias: {idade_dias}")
