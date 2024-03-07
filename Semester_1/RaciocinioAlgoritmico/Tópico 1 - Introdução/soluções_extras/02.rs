7063b7f399f5aa6e558e3082054b6b891979bfff
Eduardo D <dudzcool@gmail.com>
2024-03-07 18:35:55 +0000

    feat: Add rust solution #2

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções_extras/02.rs
@@ -0,0 +1,7 @@
+// Calcule o antecessor e o sucessor de um número
+
+fn main() {
+    let numero = 5;
+
+    println!("O antecessor de {} é {} e seu sucessor é {}", numero, numero - 1, numero + 1)
+}
