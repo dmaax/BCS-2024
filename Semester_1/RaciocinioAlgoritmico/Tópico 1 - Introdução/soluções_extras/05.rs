7212ec6953aaa84249f9abbcdbfc3265f7490957
Eduardo D <dudzcool@gmail.com>
2024-03-07 19:26:31 +0000

    feat: Add rust solution #5

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções_extras/05.rs
@@ -0,0 +1,8 @@
+// Calcule a metragem quadrada de uma área qualquer (casa, terreno, quarto, sala, etc)
+
+fn main() {
+  let largura = 10.0;
+  let comprimento = 20.0;
+
+  println!("A metragem quadrada é de {}", largura * comprimento)
+}
