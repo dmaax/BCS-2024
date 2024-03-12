7e5ca9f25addc4483c9d1d7f13fa164d0533676e
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-12 19:11:35 +0000

    feat: Add rust solution #07

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções_extras/07.rs
@@ -0,0 +1,12 @@
+//Calcule a média de idade de 5 pessoas
+
+fn main() {
+  let idade1 = 17;
+  let idade2 = 18;
+  let idade3 = 20;
+  let idade4 = 23;
+  let idade5 = 24;
+
+  let media = (idade1 + idade2 + idade3 + idade4 + idade5) / 5;
+  println!("A média de idade é {}", media);
+}
