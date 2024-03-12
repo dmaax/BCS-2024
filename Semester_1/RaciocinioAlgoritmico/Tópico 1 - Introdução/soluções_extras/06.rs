abc0b36db0e016e9493519a5cae4b64e07a2e618
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-12 19:09:23 +0000

    feat: Add rust solution #06

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 1 - Introdução/soluções_extras/06.rs
@@ -0,0 +1,9 @@
+// Calcule a metragem quadrada de uma casa com 3 pavimentos
+
+fn main() {
+  let largura = 10.0;
+  let comprimento = 20.0;
+  let pavimentos = 3.0;
+
+  println!("A metragem quadrada é de {}", largura * comprimento * pavimentos)
+}
