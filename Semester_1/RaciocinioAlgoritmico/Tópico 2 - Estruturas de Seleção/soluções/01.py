bcced0c088ce94fab3c57166cf5c79b26118437a
Eduardo D <40745408+dmaax@users.noreply.github.com>
2024-03-14 00:18:39 +0000

    feat: Add first solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 2 - Estruturas de Seleção/soluções/01.py
@@ -0,0 +1,9 @@
+# 1. Informe se uma pessoa tem o mesmo nome que você
+
+nome_a = input("Digite seu nome: ")
+nome_b = input("Digite o outro nome: ")
+
+if nome_a == nome_b:
+    print("Os dois nomes são iguais")
+else:
+    print("Os nomes não são iguais")
