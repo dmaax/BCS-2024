82ae6d891e1c581667c0c6625ef04183e4b8ae02
Eduardo Machado <dudzcool@gmail.com>
2024-03-14 11:33:59 -0300

    feat: Add seventh solution

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 2 - Estruturas de Seleção/soluções/07.py
@@ -0,0 +1,8 @@
+# Informe se um número é par (Use o módulo da divisão (%))
+
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("Esse número é par")
+else:
+    print("Esse número é impar")
