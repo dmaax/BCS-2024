caa7d1b7a1cb8ffd732f1d2168386f518d74f226
Eduardo D <dudzcool@gmail.com>
2024-03-18 14:40:42 -0300

    feat: Add rust solution #6

---
+++ Semester_1/RaciocinioAlgoritmico/Tópico 2 - Estruturas de Seleção/soluções_extras/06.rs
@@ -0,0 +1,24 @@
+// Informe se um ano é bissexto ou não.
+
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Falha ao ler linha");
+
+    let ano: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Valor inválido!");
+            return;
+        }
+    };
+
+    if ano % 4 == 0 {
+        println!("{ano} é Bissexto");
+    } else {
+        println!("{ano} não é bissexto")
+    }
+}
